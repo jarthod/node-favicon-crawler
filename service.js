c9c0675b193c1709025071837cb62d292144d973
Adrien Jarthon <jobs@adrienjarthon.com>
2012-06-06 22:12:11 +0200

    fetch favicon.ico and forward it

--- service.js
+++ service.js
@@ -1,21 +1,52 @@
 var http = require('http');
 var sys = require('util');
 var get = require('request');
+var htmlparser = require("htmlparser");
 
 http.createServer(function(request, response) {
   // Fetching hostname from url (http://localhost:3000/dashlane.com)
   var hostname = request.url.substr(1);
 
-  response.writeHead(200, {'Content-Type': 'text/plain'});
+  // Default favicon paths
+  var icons = ['/favicon.ico'];
 
-  // Load the web page
+  // Try to load the web page
   get("http://" + hostname, function(err, res, body) {
     if (!err && res.statusCode == 200) {
-      response.end("Got response: " + body);
+      // Parse the content
+      var parser = new htmlparser.Parser(new htmlparser.DefaultHandler(function (error, dom) {
+        if (error)
+          output(0);
+        else {
+          sys.puts(sys.inspect(dom));
+          output(0);
+        }
+      })).parseComplete(body);
     } else {
-      response.end("Got error: " + err);
+      output(0);
     }
   });
+
+  // Find a valid favicon and send it back
+  var output = function(i) {
+    if (icons[i]) {
+      sys.puts("Trying to fetch " + icons[i]);
+      // Try to load the icon
+      get({url: "http://" + hostname + icons[i], encoding: null}, function(err, res, body) {
+        if (!err && res.statusCode == 200) {
+          // Parse the content
+          response.writeHead(res.statusCode, res.headers);
+          response.end(body, 'binary');
+        } else {
+          output(i+1);
+        }
+      });
+    } else {
+      // None favicon left :'(
+      response.writeHead(404, {'Content-Type': 'text/plain'});
+      response.end("404 Not Found\n");
+    }
+  };
 
 }).listen(3000, '127.0.0.1');
 
--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -1,17 +1,20 @@
 require 'open-uri'
 
-describe 'service' do
+describe 'service.js' do
 
   before :all do
     @server = fork do
       exec 'node service.js'
     end
-    sleep 0.1
+    sleep 0.2
   end
 
+  let(:google_favicon) { File.read('spec/google-favicon.ico').force_encoding('ascii-8bit') }
+
   it 'should return valid favicon for google.com' do
-    open("http://localhost:3000/google.com") do |f|
-      puts f.read
+    open("http://localhost:3000/www.google.com") do |f|
+      f.content_type.should == 'image/x-icon'
+      f.read.should == google_favicon
     end
   end
 
